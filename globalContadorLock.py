06a1066e697bb2cd44bc3614ab9ea14338ba2581
javier morales <jaloazel@hotmail.com>
2021-04-24 09:05:57 -0300

    Lock incorporados

--- globalContadorLock.py
+++ globalContadorLock.py
@@ -2,12 +2,16 @@ import threading
 
 contador = 0
 
+lock = threading.Lock()
 
 def funcion():
-    global contador
-    for i in range(1000000):
-        contador += 1
-
+    lock.acquire()
+    try:
+        global contador
+        for i in range(1000000):
+            contador += 1
+    finally:
+        lock.release()
 
 print("Inicio programa principal")
 print("Valor Inicial: " + str(contador))
@@ -25,7 +29,3 @@ thread_2.join()
 thread_3.join()
 
 print("Valor Final: " + str(contador))
-
-
-
-
